9f1300304468b70741b0d74077adbf7a48718b18
Yash Chaturvedi <chaturvediyash624@gmail.com>
2024-06-01 23:04:13 +0530

    Initial commit

---
+++ my-plugin.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * Plugin Name: Yash Website Custom Plugin
+ * Description: Handle the custom functions for the yashchaturvedi.great-site.net
+ */
+add_filter('show_admin_bar', '__return_false');
